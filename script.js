6027b67e1c3a567d5f5d0eb170e4e8ba99b31e74
Aaron <awitthohn@yahoo.com>
2023-02-18 14:11:29 -0500

    added button feature, timer now activates on button

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
+//Timer Element
 const time = document.querySelector('h1');
-let timeSecond = 5;
+let timeSecond = 10;
 
 time.innerHTML = `Time Remaining:  ${timeSecond}`;
 
-const countDown = setInterval (()=>{
-    timeSecond--;
-    time.innerHTML = `Time Remaining:  ${timeSecond}`;
-    if(timeSecond <= 0 || timeSecond < 1){
-        clearInterval(countDown)
-    }
-},1000)
+const start= document.getElementById(`start-game`);
+
+start.addEventListener(`click`,function(){
+    const countDown = setInterval (()=>{
+        timeSecond--;
+        time.innerHTML = `Time Remaining:  ${timeSecond}`;
+        if(timeSecond <= 0 || timeSecond < 1){
+            clearInterval(countDown)
+        } 
+    },1000)
+} )
